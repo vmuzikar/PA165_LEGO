9371c80636b9966debd59f121c9136367961116c
vmuzikar <vmuzikar@redhat.com>
2015-11-02 15:30:51 +0100

    Spring, Hibernate config

--- model/src/test/java/org/legomanager/AppTest.java
+++ model/src/test/java/org/legomanager/AppTest.java
@@ -1,38 +1,20 @@
 package org.legomanager;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@ContextConfiguration("classpath:META-INF/spring-config.xml")
+public class AppTest extends AbstractTestNGSpringContextTests
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+    @PersistenceContext
+    EntityManager entityManager;
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void justTesting() {
+        logger.info("Hello World!");
     }
 }
