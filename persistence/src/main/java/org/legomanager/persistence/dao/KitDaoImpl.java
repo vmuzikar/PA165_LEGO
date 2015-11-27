87a20fe1f300b9b2bc03425d7de49374920adf21
vmuzikar <vaclav@muzikari.cz>
2015-11-27 20:07:22 +0100

    Fix getKitsWithAgeRange algorithm

--- persistence/src/main/java/org/legomanager/persistence/dao/KitDaoImpl.java
+++ persistence/src/main/java/org/legomanager/persistence/dao/KitDaoImpl.java
@@ -17,9 +17,9 @@ public class KitDaoImpl extends AbstractBaseDaoImpl<Kit> implements KitDao {
 
     @Override
     public List<Kit> getKitsWithAgeRange(short ageFrom, short ageTo) {
-        TypedQuery<Kit> query = em.createQuery("SELECT k FROM Kit k WHERE k.minAge >= :minAge AND k.maxAge <= :maxAge", Kit.class);
-        query.setParameter("maxAge", ageFrom);
-        query.setParameter("minAge", ageTo);
+        TypedQuery<Kit> query = em.createQuery("SELECT k FROM Kit k WHERE k.minAge <= :maxAge AND k.maxAge >= :minAge", Kit.class);
+        query.setParameter("minAge", ageFrom);
+        query.setParameter("maxAge", ageTo);
         return query.getResultList();
     }
 }
